2454eef2b17e6d139e11de42922eeaa4361a9119
KTH <johan1103@naver.com>
2022-08-04 13:11:45 +0900

    edit RestControllerTest.java
    sout지우기

--- NMT/src/main/java/SWM_RM/NMT/controller/RestControllerTest.java
+++ NMT/src/main/java/SWM_RM/NMT/controller/RestControllerTest.java
@@ -24,7 +24,6 @@ public class RestControllerTest {
     public String testController(@RequestBody DbTestDTO dbTestDTO){
         System.out.println(dbTestDTO.getName());
         System.out.println(dbTestDTO.getAge());
-        System.out.println("hello world!");
         //return "ok";
         return dbTestRepository.createDbTest(dbTestDTO);
     }
