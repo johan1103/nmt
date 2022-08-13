0f5d074c40328079af6aa8e66ec703ef0036423e
KTH <johan1103@naver.com>
2022-08-13 11:53:06 +0900

    add UserUniversityTest
    테스트코드 추가

---
+++ NMT/src/test/java/SWM_RM/NMT/domain/UserUniversityTest.java
@@ -0,0 +1,45 @@
+package SWM_RM.NMT.domain;
+
+import SWM_RM.NMT.domain.compositeKey.UserUniversityPK;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class UserUniversityTest {
+    @Autowired
+    private EntityManager em;
+    @Test
+    @Transactional
+    @Rollback(value = true)
+    public void createUserUniversity() throws Exception {
+        User user = new User();
+        University university = new University();
+        UserUniversity userUniversity = new UserUniversity();
+        UserUniversityPK userUniversityPK = new UserUniversityPK();
+        user.setNickname("kimmm");
+        user.setStrick("12111");
+        university.setUniversityName("InhaUniv");
+        userUniversityPK.setUniversityId(university);
+        userUniversityPK.setUserId(user);
+        userUniversity.setUniversityPK(userUniversityPK);
+        userUniversity.setInterest(true);
+        em.persist(user);
+        em.persist(university);
+        em.persist(userUniversity);
+
+        em.flush();
+        em.clear();
+
+        System.out.println();
+    }
+}
